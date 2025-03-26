3ac692ef4018a704b6f8ea93b66112a6d7904aa9
Chamath Nimnada <nimnadachamath25@gmail.com>
2025-03-26 10:46:05 +0530

    Add project files.

---
+++ Port-Checker/HompePage.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Port_Checker
+{
+    partial class PortCheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PortCheck));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lstBusyPorts = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstFreePorts = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(34, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the Port Number\r\n";
+            // 
+            // txtPort
+            // 
+            this.txtPort.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(261, 46);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(203, 30);
+            this.txtPort.TabIndex = 1;
+            // 
+            // lstBusyPorts
+            // 
+            this.lstBusyPorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBusyPorts.FormattingEnabled = true;
+            this.lstBusyPorts.ItemHeight = 25;
+            this.lstBusyPorts.Location = new System.Drawing.Point(12, 245);
+            this.lstBusyPorts.Name = "lstBusyPorts";
+            this.lstBusyPorts.Size = new System.Drawing.Size(307, 204);
+            this.lstBusyPorts.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(470, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(631, 107);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Port";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(182, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(461, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Load All Port";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkRed;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(12, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(23, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(284, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "------------ Busy Ports ------------";
+            // 
+            // lstFreePorts
+            // 
+            this.lstFreePorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFreePorts.FormattingEnabled = true;
+            this.lstFreePorts.ItemHeight = 25;
+            this.lstFreePorts.Location = new System.Drawing.Point(336, 245);
+            this.lstFreePorts.Name = "lstFreePorts";
+            this.lstFreePorts.Size = new System.Drawing.Size(307, 204);
+            this.lstFreePorts.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(345, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(280, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "------------ Free Ports ------------";
+            // 
+            // PortCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(659, 466);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstFreePorts);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lstBusyPorts);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PortCheck";
+            this.Text = "Port Checker";
+            this.Load += new System.EventHandler(this.PortCheck_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.ListBox lstBusyPorts;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstFreePorts;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Port-Checker/HompePage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+
+namespace Port_Checker
+{
+    public partial class PortCheck : Form
+    {
+        public PortCheck()
+        {
+            InitializeComponent();
+            LoaddAllPorts();
+        }
+
+        private bool IsPortInUse(int port)
+        {
+            var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnections = ipProperties.GetActiveTcpListeners();
+            var udpConnections = ipProperties.GetActiveUdpListeners();
+
+            return tcpConnections.Any(t => t.Port == port) || udpConnections.Any(u => u.Port == port);
+        }
+
+
+        private void PortCheck_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtPort.Text, out int port))
+            {
+                bool inUse = IsPortInUse(port);
+                MessageBox.Show($"Port {port} is {(inUse ? "In Use" : "Free")}.", "Port Status");
+                txtPort.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid port number.", "Error");
+                txtPort.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoaddAllPorts();
+        }
+
+        private void LoaddAllPorts() 
+        {
+            lstBusyPorts.Items.Clear();
+            lstFreePorts.Items.Clear();
+
+            var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnections = ipProperties.GetActiveTcpListeners().Select(t => t.Port).ToList();
+            var udpConnections = ipProperties.GetActiveUdpListeners().Select(u => u.Port).ToList();
+
+            var busyPorts = tcpConnections.Concat(udpConnections).Distinct().ToList();
+            var allPorts = Enumerable.Range(1, 65535);
+            var freePorts = allPorts.Except(busyPorts).OrderBy(_ => Guid.NewGuid()).Take(100); // Random 100 free ports
+
+            foreach (var port in busyPorts)
+            {
+                lstBusyPorts.Items.Add($"Port {port} - In Use");
+            }
+
+            foreach (var port in freePorts)
+            {
+                lstFreePorts.Items.Add($"Port {port} - Free");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoaddAllPorts();
+        }
+    }
+}
